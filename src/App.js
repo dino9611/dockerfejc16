4ce955473022927eddc9853c8aabbc84ec4b7820
dino9611 <aldinorahman36@gmail.com>
2021-06-03 10:15:21 +0700

    udpate

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to dino.
+          Edit <code>src/App.js</code> and save to dino JC1604.
         </p>
         <a
           className="App-link"
